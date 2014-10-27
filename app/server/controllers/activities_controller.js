065580c1dccae6a9ca429e29520cc0b25d965d0a
Charles Pletcher <cpletcher2@gmail.com>
2014-10-27 16:01:14 -0700

    Automatically link updates to Assembly products

--- app/server/controllers/activities_controller.js
+++ app/server/controllers/activities_controller.js
@@ -22,6 +22,7 @@ activities.route(route)
 })
 .post(function activitiesPOST(req, res, next) {
   var product = req.params.product;
+  var token = req.query.token;
   var body = req.body;
 
   var activity = new Activity({
@@ -33,7 +34,7 @@ activities.route(route)
   })
   .save()
   .then(function(a) {
-    checkUpdates(product);
+    checkUpdates(product, token);
     res.status(201).json(a);
   })
   .otherwise(function(err) {
@@ -42,7 +43,35 @@ activities.route(route)
   });
 });
 
-function alertProduct(count, product, timestamp) {
+function alertProduct(count, product, token, timestamp) {
+  // Assembly-only
+  var endpoint = process.env.ASSEMBLY_API + '/' + product + '/activities?token=' + token;
+  var countDays = timestamp > 0 ? Math.floor((Date.now() - timestamp) / ONE_DAY) : 1;
+  var days = countDays === 1 ? 'the past day' : 'the past' + countDays + ' days';
+  var message = 'There were ' + count + ' new signups in ' + days + '.';
+
+  request({
+    method: 'POST',
+    uri: endpoint,
+    body: {
+      news_feed_item: {
+        message: message
+      },
+      user_token: process.env.ASSEMBLY_AUTHENTICATION_TOKEN
+    },
+    json: true
+  }, function(err, response, body) {
+    if (err) {
+      console.log('Error sending message to ' + endpoint);
+    }
+
+    console.log(body);
+  });
+
+  alertSubscribers(count, product, timestamp);
+}
+
+function alertSubscribers(count, product, timestamp) {
   Subscriber.where({ product: product })
   .fetchAll()
   .then(function(subscribers) {
@@ -77,20 +106,20 @@ function alertProduct(count, product, timestamp) {
   });
 }
 
-function checkUpdates(product) {
+function checkUpdates(product, token) {
   Update.where({ product: product })
   .fetch()
   .then(function(update) {
     var now = Date.now();
 
     if (!update) {
-      return countActivitiesSince(product);
+      return countActivitiesSince(product, token);
     }
 
     if (now - update.get('sent_at') > ONE_DAY &&
         // don't check more than once per day
         now - update.get('checked_at') > ONE_DAY) {
-      countActivitiesSince(product, update);
+      countActivitiesSince(product, token, update);
     }
   })
   .otherwise(function(error) {
@@ -99,7 +128,7 @@ function checkUpdates(product) {
   });
 }
 
-function countActivitiesSince(product, update) {
+function countActivitiesSince(product, token, update) {
   if (!update) {
     update = new Update({
       product: product,
@@ -118,7 +147,7 @@ function countActivitiesSince(product, update) {
     var now = new Date();
 
     if (count >= COUNT) {
-      alertProduct(count, product, timestamp);
+      alertProduct(count, product, token, timestamp);
       return markUpdate(update, {
         sent_at: now,
         current_count: 0
